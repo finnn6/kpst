ca83577d7f0617ceed3d6d74e97b63c7bbc2d06e
lim <aodco213@gmail.com>
2021-07-01 09:39:13 +0900

    플라스크 함수생성

--- KpstPythonDatabaseFlask/DBconn.py
+++ KpstPythonDatabaseFlask/DBconn.py
@@ -32,3 +32,353 @@ def logIn(sql):
     except mysql.connector.Error as err:
 
         print(err)
+
+
+def createEmp(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def checkid(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def updateEmp(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def getAllEmp(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def getKeywordRank(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def increaseKeywordCnt(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def insertKeyword(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def searchKeyword(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def getRecentStudyTime(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def getTodayStudyTime(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def insertStudyEndTime(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def insertStudyStartTime(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def selectAllStudyTime(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
+
+
+def studyStatus(sql):
+    try:
+        conn = mysql.connector.connect(**config)
+        print(conn)
+        cursor = conn.cursor()
+        sql
+        total = cursor.execute(sql)
+        rere = cursor.fetchall()
+        print(rere)
+        print(total)
+    # for result in resultList:
+    #     EMP_ID = result[0]
+    #     PWD = result[1]
+    #     NAME = result[2]
+    #     BIR = result[3]
+    #     PH = result[4]
+    #     INTROD_ID = result[5]
+    #     info = "EMP_ID:{},PWD:{},NAME:{},BIR:{},PH{},INTROD_ID{}".format(EMP_ID,PWD,NAME,BIR,PH,INTROD_ID)
+    #
+    #     print(info)
+    except mysql.connector.Error as err:
+
+        print(err)
--- KpstPythonDatabaseFlask/flaskAPI.py
+++ KpstPythonDatabaseFlask/flaskAPI.py
@@ -1,6 +1,6 @@
 from flask import Flask, session, render_template, redirect, request, url_for
 
-import DBconn
+import test
 
 app = Flask(__name__)
 
@@ -8,10 +8,85 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
-@app.route('/logIn')
+@app.route('/login')
 def login():
     # id = request.get
-    DBconn.logIn(sql="SELECT COUNT(EMP_ID)  FROM TBL_EMP te WHERE EMP_ID = 'sys'and PWD ='123123';")
+    test.logIn(sql="SELECT COUNT(EMP_ID)  FROM TBL_EMP te WHERE EMP_ID = 'sys'and PWD ='123123';")
+    return
+
+@app.route('/createEmp')
+def createEmp():
+
+    return
+
+@app.route('/checkid')
+def chekid():
+
+    return
+
+@app.route('/updateEmp')
+def updateEmp():
+
+    return
+
+@app.route('/getAllEmp')
+def getAllEmp():
+
+    return
+
+@app.route('/deleteEmp')
+def deleteEmp():
+
+    return
+
+@app.route('/getKeywordRank')
+def getKeywordRank():
+
+    return
+
+@app.route('/increaseKeywordCnt')
+def increaseKeywordCnt():
+
+    return
+
+@app.route('/insertKeyword')
+def insertKeyword():
+
+    return
+
+@app.route('/searchKeyword')
+def searchKeyword():
+
+    return
+
+@app.route('/getRecentStudyTime')
+def getKeywordRank():
+
+    return
+
+@app.route('/getTodayStudyTime')
+def getKeywordRank():
+
+    return
+
+@app.route('/insertStudyEndTime')
+def getKeywordRank():
+
+    return
+
+@app.route('/insertStudyStartTime')
+def getKeywordRank():
+
+    return
+
+@app.route('/selectAllStudyTime')
+def getKeywordRank():
+
+    return
+
+@app.route('/studyStatus')
+def getKeywordRank():
+
     return
 
 if __name__ == '__main__':
